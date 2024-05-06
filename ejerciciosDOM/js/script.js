7a3a07f5fa0b9b341d75d91cbb7f576014e64b23
Carlos Fernandez <krlosafernandez@gmail.com>
2024-05-06 17:31:15 -0500

    empece el modificar el archivo js

---
+++ ejerciciosDOM/js/script.js
@@ -0,0 +1,5 @@
+let mensagem = document.getElementById('resultado');
+let peso = document.getElementById('peso');
+let altura = document.getElementById('estatura');
+let btn = document.getElementById('calcular');
+let error = document.getElementById('error');
