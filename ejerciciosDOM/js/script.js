9e4ad0866d9523acb6de5cb70d0c2a9bd829970c
Carlos Fernandez <krlosafernandez@gmail.com>
2024-05-07 11:23:06 -0500

    empece frontend de  ejercicio 3

--- ejerciciosDOM/js/script.js
+++ ejerciciosDOM/js/script.js
@@ -1,5 +1,17 @@
-let mensagem = document.getElementById('resultado');
-let peso = document.getElementById('peso');
-let altura = document.getElementById('estatura');
-let btn = document.getElementById('calcular');
-let error = document.getElementById('error');
+let mensagem = document.getElementById("resultado");
+let peso = document.getElementById("peso");
+let altura = document.getElementById("estatura");
+let btn = document.getElementById("calcular");
+let error = document.getElementById("error");
+
+
+btn.addEventListener("click", function () {
+
+    let imc = peso.value / (estatura.value * estatura.value);
+    resultado.innerText = "Su IMC es: " + imc
+    peso.value = ""
+    estatura.value = ""
+
+})
+
+
